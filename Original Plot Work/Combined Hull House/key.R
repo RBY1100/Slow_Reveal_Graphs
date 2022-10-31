2fcc6d0021ea6702e9b981209c3a928bcdeb4721
Robert <rby1100@gmail.com>
2022-10-31 14:14:39 -0400

    color work

--- Original Plot Work/Combined Hull House/key.R
+++ Original Plot Work/Combined Hull House/key.R
@@ -1,5 +1,29 @@
-red: c05a50
-red 2: FF2626
-red 3: FF7575
-red 4: FFA6A6
-bleeched red: FFD3D3
+red: 942626
+red 2: a63838
+red 3: b54e4e
+red 4: bf6262
+bleeched red: d17d7d
+
+yellow: 949026
+yellow 2: a6a238
+yellow 3: b5b54e
+yellow 4: bcbf62
+bleeched yellow: cbd17d
+
+purple:7c2694
+purple 2:883299
+purple 3:944eb5
+purple 4:ab62bf
+bleeched purple:c97dd1
+
+blue:3c2694
+blue 2:323299
+blue 3:554eb5
+blue 4:6462bf
+bleeched blue:7d8bd1
+
+green:
+green 2:
+green 3:
+green 4:
+bleeched green:
