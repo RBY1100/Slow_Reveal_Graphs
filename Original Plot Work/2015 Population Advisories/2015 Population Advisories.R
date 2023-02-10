b86e22d88bfe47c653378ffef5a00288afe277e4
Robert <RBY1100@gmail.com>
2023-02-10 18:57:16 -0500

    more finalizations

--- Original Plot Work/2015 Population Advisories/2015 Population Advisories.R
+++ Original Plot Work/2015 Population Advisories/2015 Population Advisories.R
@@ -20,6 +20,7 @@ data$Type <- as.factor(data$Type)
 
 
 
+#Final Graph
 
 graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, color=Type, alpha = .5, size=1.75)) +
   labs(title = "Long-term Crinking Water Advisories",
@@ -42,7 +43,7 @@ graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, color=Type, 
 
 gap <- ggplot() + theme_void()
 
-png(here::here("Original Plot Work", "2015 Population Advisories", "2015 Population Advisories U.png"),width=700,height=500)
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "6 Final 2015 Population Advisories U.png"),width=700,height=500)
 ggarrange(gap,
           ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
           gap, ncol = 3, widths = c(.05,1,.1))
@@ -50,7 +51,7 @@ dev.off()
 
 
 
-original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "2015 Population Advisories U.png"))
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "6 Final 2015 Population Advisories U.png"))
 
 
 img <- image_draw(original)
@@ -67,9 +68,239 @@ polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "whi
 dev.off()
 
 
-image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "2015 Population Advisories.png"), format = "png")
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "6 Final 2015 Population Advisories.png"), format = "png")
 
 
+
+#First  Graph
+
+graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, size=1.75)) +
+  labs(title = "Long-term Crinking Water Advisories",
+       subtitle = "affecting First Nations reserves (snapshot Dec 2015)",
+       y = "Population affected",
+       x = "Length of incident (years)") +
+  theme(legend.position = "none",
+        plot.title = element_text(color="white", size=16, hjust=.45),
+        plot.subtitle = element_text(color="white", size=16),
+        axis.title = element_text(size=11, color="white"),
+        axis.text = element_text(size=12, color="white"),
+        axis.ticks = element_line(linewidth = 1),
+        axis.ticks.length=unit(.2, "cm"),
+        panel.grid.major = element_line(color="black"),
+        panel.grid.minor = element_line(color="gray"),
+        panel.background = element_rect(color="gray" ,fill = "white"))
+
+
+gap <- ggplot() + theme_void()
+
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "1 2015 Population Advisories U.png"),width=700,height=500)
+ggarrange(gap,
+          ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
+          gap, ncol = 3, widths = c(.05,1,.1))
+dev.off()
+
+
+
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "1 2015 Population Advisories U.png"))
+
+
+img <- image_draw(original)
+
+
+polygon(c(90, 635, 635, 90), c(97, 97, 90, 90), border = "white", col = "white")
+polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "white")
+
+
+dev.off()
+
+
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "1 2015 Population Advisories.png"), format = "png")
+
+
+
+#Second  Graph
+
+graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, size=1.75)) +
+  labs(title = "Long-term Crinking Water Advisories",
+       subtitle = "affecting First Nations reserves (snapshot Dec 2015)",
+       y = "Population affected",
+       x = "Length of incident (years)") +
+  theme(legend.position = "none",
+        plot.title = element_text(color="white", size=16, hjust=.45),
+        plot.subtitle = element_text(color="white", size=16),
+        axis.title.x = element_text(size=11, color="black"),
+        axis.title.y = element_text(size=11, color="white"),
+        axis.text.x = element_text(size=12, color="black"),
+        axis.text.y = element_text(size=12, color="white"),
+        axis.ticks = element_line(linewidth = 1),
+        axis.ticks.length=unit(.2, "cm"),
+        panel.grid.major = element_line(color="black"),
+        panel.grid.minor = element_line(color="gray"),
+        panel.background = element_rect(color="gray" ,fill = "white"))
+
+
+gap <- ggplot() + theme_void()
+
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "2 2015 Population Advisories U.png"),width=700,height=500)
+ggarrange(gap,
+          ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
+          gap, ncol = 3, widths = c(.05,1,.1))
+dev.off()
+
+
+
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "2 2015 Population Advisories U.png"))
+
+
+img <- image_draw(original)
+
+
+polygon(c(90, 635, 635, 90), c(97, 97, 90, 90), border = "white", col = "white")
+polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "white")
+
+
+dev.off()
+
+
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "2 2015 Population Advisories.png"), format = "png")
+
+
+
+#Third  Graph
+
+graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, size=1.75)) +
+  labs(title = "Long-term Crinking Water Advisories",
+       subtitle = "affecting First Nations reserves (snapshot Dec 2015)",
+       y = "Population affected",
+       x = "Length of incident (years)") +
+  theme(legend.position = "none",
+        plot.title = element_text(color="white", size=16, hjust=.45),
+        plot.subtitle = element_text(color="white", size=16),
+        axis.title = element_text(size=11, color="black"),
+        axis.text = element_text(size=12, color="black"),
+        axis.ticks = element_line(linewidth = 1),
+        axis.ticks.length=unit(.2, "cm"),
+        panel.grid.major = element_line(color="black"),
+        panel.grid.minor = element_line(color="gray"),
+        panel.background = element_rect(color="gray" ,fill = "white"))
+
+
+gap <- ggplot() + theme_void()
+
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "3 2015 Population Advisories U.png"),width=700,height=500)
+ggarrange(gap,
+          ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
+          gap, ncol = 3, widths = c(.05,1,.1))
+dev.off()
+
+
+
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "3 2015 Population Advisories U.png"))
+
+
+img <- image_draw(original)
+
+
+polygon(c(90, 635, 635, 90), c(97, 97, 90, 90), border = "white", col = "white")
+polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "white")
+
+
+dev.off()
+
+
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "3 2015 Population Advisories.png"), format = "png")
+
+
+
+#Fourth  Graph
+
+graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, size=1.75)) +
+  labs(title = "Long-term Crinking Water Advisories",
+       subtitle = "affecting First Nations reserves (snapshot Dec 2015)",
+       y = "Population affected",
+       x = "Length of incident (years)") +
+  theme(legend.position = "none",
+        plot.title = element_text(color="white", size=16, hjust=.45),
+        plot.subtitle = element_text(color="black", size=16),
+        axis.title = element_text(size=11, color="black"),
+        axis.text = element_text(size=12, color="black"),
+        axis.ticks = element_line(linewidth = 1),
+        axis.ticks.length=unit(.2, "cm"),
+        panel.grid.major = element_line(color="black"),
+        panel.grid.minor = element_line(color="gray"),
+        panel.background = element_rect(color="gray" ,fill = "white"))
+
+
+gap <- ggplot() + theme_void()
+
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "4 2015 Population Advisories U.png"),width=700,height=500)
+ggarrange(gap,
+          ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
+          gap, ncol = 3, widths = c(.05,1,.1))
+dev.off()
+
+
+
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "4 2015 Population Advisories U.png"))
+
+
+img <- image_draw(original)
+
+
+polygon(c(90, 635, 635, 90), c(97, 97, 90, 90), border = "white", col = "white")
+polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "white")
+
+
+dev.off()
+
+
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "5 2015 Population Advisories.png"), format = "png")
+
+
+
+#Fifth  Graph
+
+graph <- ggplot(data=data) + geom_point(aes(x=Length, y=Population, size=1.75)) +
+  labs(title = "Long-term Crinking Water Advisories",
+       subtitle = "affecting First Nations reserves (snapshot Dec 2015)",
+       y = "Population affected",
+       x = "Length of incident (years)") +
+  theme(legend.position = "none",
+        plot.title = element_text(color="black", size=16, hjust=.45),
+        plot.subtitle = element_text(color="black", size=16),
+        axis.title = element_text(size=11, color="black"),
+        axis.text = element_text(size=12, color="black"),
+        axis.ticks = element_line(linewidth = 1),
+        axis.ticks.length=unit(.2, "cm"),
+        panel.grid.major = element_line(color="black"),
+        panel.grid.minor = element_line(color="gray"),
+        panel.background = element_rect(color="gray" ,fill = "white"))
+
+
+gap <- ggplot() + theme_void()
+
+png(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "5 2015 Population Advisories U.png"),width=700,height=500)
+ggarrange(gap,
+          ggarrange(gap, graph, gap, nrow = 3, heights = c(.1, 1, .15)),
+          gap, ncol = 3, widths = c(.05,1,.1))
+dev.off()
+
+
+
+original <- image_read(here::here("Original Plot Work", "2015 Population Advisories", "Unfinished Graphs", "5 2015 Population Advisories U.png"))
+
+
+img <- image_draw(original)
+
+
+polygon(c(90, 635, 635, 90), c(97, 97, 90, 90), border = "white", col = "white")
+polygon(c(630, 635, 635, 630), c(480, 480, 90, 90), border = "white", col = "white")
+
+
+dev.off()
+
+
+image_write(img, path = here::here("Original Plot Work", "2015 Population Advisories", "5 2015 Population Advisories.png"), format = "png")
 
 
 
