bec8d90ee10c5a68b10aeaa842cb327b6062a105
Robert <RBY1100@gmail.com>
2023-04-05 11:57:08 -0400

    adjusting the domestication of animals graph

--- Original Plot Work/Domestication of Animals/Domestication of Animals.R
+++ Original Plot Work/Domestication of Animals/Domestication of Animals.R
@@ -71,6 +71,65 @@ graph <- ggplot(animals) +
         plot.background = element_rect(color="#f2ece7", fill="#f2ece7"),
         panel.background = element_rect(color="#f2ece7", fill="#f2ece7"))
 
+
+
+
+
+
+graph1 <- ggplot(animals) + 
+  
+  geom_arcbar(aes(shares = blank, r0 = 0, r1 = -32, color = type, fill = type), sep=0) + 
+  scale_fill_manual(values =c("white","white")) +
+  scale_color_manual(values =c("white","white")) +
+  new_scale_color() + 
+  new_scale_fill() + 
+  
+  
+  geom_arcbar(aes(shares = blank, r0 = -8.99, r1 = -9.01, color = type, fill = type), sep=0) +
+  scale_fill_manual(values =c("black","black")) +
+  scale_color_manual(values =c("black","black")) +
+  new_scale_color() + 
+  new_scale_fill() +
+  
+  geom_arcbar(aes(shares = duck, r0 = -9.5, r1 = -10.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = turkey, r0 = -11, r1 = -12, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = a_camel, r0 = -12.5, r1 = -13.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = chicken, r0 = -14, r1 = -15, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = b_camel, r0 = -15.5, r1 = -16.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = alpaca, r0 = -17, r1 = -18, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = horse, r0 = -18.5, r1 = -19.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = llama, r0 = -20, r1 = -21, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = zebu, r0 = -21.5, r1 = -22.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = pig_cow, r0 = -24.5, r1 = -25.5, color = type, fill = type), sep=0) +
+  geom_arcbar(aes(shares = pig_cow, r0 = -26, r1 = -27, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = goat, r0 = -27.5, r1 = -28.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = sheep, r0 = -29, r1 = -30, color = type, fill = type), sep=0) + 
+  scale_fill_manual(values =c("white","#e85d41")) +
+  scale_color_manual(values =c("white","#e85d41")) +
+  new_scale_color() + 
+  new_scale_fill() + 
+  
+  geom_arcbar(aes(shares = dog, r0 = -30.5, r1 = -31.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = cat, r0 = -23, r1 = -24, color = type, fill = type), sep=0) + 
+  scale_fill_manual(values =c("white","#359dbc")) +
+  scale_color_manual(values =c("white","#359dbc")) +
+  new_scale_color() + 
+  new_scale_fill() + 
+  
+  geom_arcbar(aes(shares = a_camel, r0 = -12.5, r1 = -13.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = b_camel, r0 = -15.5, r1 = -16.5, color = type, fill = type), sep=0) + 
+  geom_arcbar(aes(shares = horse, r0 = -18.5, r1 = -19.5, color = type, fill = type), sep=0) + 
+  scale_fill_manual(values =c("white","#dfcfb2")) +
+  scale_color_manual(values =c("white","#dfcfb2"))+
+  
+  coord_flip() +
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        plot.background = element_rect(color="#f2ece7", fill="#f2ece7"),
+        panel.background = element_rect(color="#f2ece7", fill="#f2ece7"))
+
 gap <- ggplot() + theme_void() + theme(plot.background = element_rect(fill="#f2ece7", color="#f2ece7"))
 
 
@@ -78,7 +137,12 @@ png(here::here("Original Plot Work", "Domestication of Animals", "temp.png"),wid
 ggarrange(gap, ggarrange(gap, graph, gap, ncol = 3, widths = c(.4, 1, .02)), gap, nrow = 3, heights = c(.15, 1, .3))
 dev.off()
 
+png(here::here("Original Plot Work", "Domestication of Animals", "temp1.png"),width=500,height=900)
+ggarrange(gap, ggarrange(gap, graph1, gap, ncol = 3, widths = c(.4, 1, .02)), gap, nrow = 3, heights = c(.15, 1, .3))
+dev.off()
+
 original <- image_read(here::here("Original Plot Work", "Domestication of Animals", "temp.png"))
+original1 <- image_read(here::here("Original Plot Work", "Domestication of Animals", "temp1.png"))
 cat <- image_read(here::here("Original Plot Work", "Domestication of Animals", "Images", "cat.png"))
 bottomrib <- image_read(here::here("Original Plot Work", "Domestication of Animals", "Images", "bottomribbon.png"))
 camels <- image_read(here::here("Original Plot Work", "Domestication of Animals", "Images", "camels.png"))
@@ -351,7 +415,7 @@ image_write(img, path = here::here("Original Plot Work", "Domestication of Anima
 
 
 
-img <- image_draw(original)
+img <- image_draw(original1)
 
 segments(x0=473,y0=325, x1=473, y1=125, lwd = 1, lty = "solid")
 
@@ -469,7 +533,7 @@ image_write(img, path = here::here("Original Plot Work", "Domestication of Anima
 
 
 
-img <- image_draw(original)
+img <- image_draw(original1)
 
 segments(x0=473,y0=325, x1=473, y1=125, lwd = 1, lty = "solid")
 
@@ -589,7 +653,7 @@ image_write(img, path = here::here("Original Plot Work", "Domestication of Anima
 
 
 
-img <- image_draw(original)
+img <- image_draw(original1)
 
 segments(x0=473,y0=325, x1=473, y1=125, lwd = 1, lty = "solid")
 
